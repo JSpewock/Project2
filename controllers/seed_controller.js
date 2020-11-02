9f60923d1f3fa73eeffa707f77018275cf997f50
Jeremy <jeremy.spewock@gmail.com>
2020-11-02 11:09:03 -0500

    added w02d01

--- controllers/seed_controller.js
+++ controllers/seed_controller.js
@@ -29,16 +29,26 @@ router.get('/', (req,res)=> {
             {
                 day: 'w01d04', 
                 summary: 'HTML Mockup Pt.1 (designing HTML for a website based off an image), JavaScript Functions, Scoping, const and let variables.',
-                description: 'The first thing we did on this day was our HTML mockup for our monring exercise. We were givin and image of a webpage and asked to design the HTML for it in our own file. Following this, in the morning lesson, we went over functions in our morning lesson. Functions are a very large topic and they\'re incredibly useful in JS. We learned how to write them, name them, call them, and set parameters for them. Later on, in the afternoon lesson, we went over scope. Scope is essentially where you can access variables in your JavaScript. Javascript as some set limitations to where certain vairables can be used depending on where they are declared so this is something we went over in this lesson. We also learned the difference between "const" and "let" variables and how to use each.',
+                description: 'The first thing we did on this day was our HTML mockup for our morning exercise. We were givin and image of a webpage and asked to design the HTML for it in our own file. Following this, in the morning lesson, we went over functions in our morning lesson. Functions are a very large topic and they\'re incredibly useful in JS. We learned how to write them, name them, call them, and set parameters for them. Later on, in the afternoon lesson, we went over scope. Scope is essentially where you can access variables in your JavaScript. Javascript as some set limitations to where certain vairables can be used depending on where they are declared so this is something we went over in this lesson. We also learned the difference between "const" and "let" variables and how to use each.',
                 lessonNotes: 'https://git.generalassemb.ly/seir-9-21/student-resources/tree/master/1_front_end_development/w01d04',
                 recording: 'https://generalassembly.zoom.us/rec/share/QCnhj1DuF3YogLIUpUnhUhb_rqvTWK9irXfgKaZg-ndJF5W1lzeZGvZrzl4fFZbZ.b486qeRHcoIeyRRh',
             },
             {
                 day: 'w01d05',
-                summary: '',
-                description: '',
-                lessonNotes: '',
-                recording: ''
+                summary: 'HTML Mockup Pt.2 (adding CSS to our Pt.1 mockup based off an image of a webpage), Psuedo code, Execute JS in a webpage, Browser Alerts, Browser Prompts, Nested Functions, Loops and Functions, Sequence of Execution, When to use a Function, Objects, Object Properties',
+                description: 'There is a lot to unpack in this day. It all starts with a morning exercise of HTML Mockup Pt.2. In this exercise we simply added CSS to our existing HTML that we made in the previous part on the day before. Then, we got into "Program Design." This morning lecture gave us a massive overview of Psuedo code, JavaScript, how to use and execute JavaScript in a browser, browser alerts, browser prompts, nested functions, reset state, sequence of execution, loops and functions, and why to use Javascript. So, in short, we essentially went over JavaScript and why we use it in more detail, how to interact with users in the browser, functions nested inside of other functions, and how loops and functions interact with each other. In the afternoon, we were introduced to objects. We learned the difference between objects and arrays and why we would use one or the other. We also discusses how to use objects, manipulate objects, and add/change object properties.',
+                lessonNotes: 'https://git.generalassemb.ly/seir-9-21/student-resources/tree/master/1_front_end_development/w01d05',
+                recording: 'https://generalassembly.zoom.us/rec/share/4I2JQyPZsXQuCGIZRxGbaXekcnCwikrZrAP7AgJo1ybA2K7qoL-z5TZKvwX5B2le.Kmhi7bb7hUt8y_fi'
+            },
+            {
+                day: 'w02d01',
+                summary: 'Beginner\'s Guide to UX/UI, Object-ception (the different datatypes in objects), Combining Datatypes',
+                description: 'This lesson was very focused on objects and arrays. First, in the morning exercise, we went over UX/UI (User experience and User Interactions). This was a fairly brief exercise where we went over the basics of how to enhance the user experience of our apps and what cluttered bad UX looked like. Next, in the morning lesson, we went over "Object-ception." Essentially, we discussed how to use different datatypes like arrays and functions inside of an object. We went over how to properly nest them, how to access them, how to iterate over arrays in objects, how to iterate over objects inside an object, how to use an object within an array, and just how to combine objects, arrays, and functions overall. Next, in the afternoon, our lesson discussed looping with nexted arrays/objects and combining objects, arrays, and functions more than one level deep. This covered things like how to access specific items in objects or arrays returned from functions.',
+                lessonNotes: 'https://git.generalassemb.ly/seir-9-21/student-resources/tree/master/1_front_end_development/w02d01',
+                recording: 'https://generalassembly.zoom.us/rec/share/FPQdmvdSp1_mRgdy2zkVeCWFcjYCVg1COoQAjih9iWMTZs7HtVjfeTqpo5zSigHB.tiKqoEZEWXHUsLyM'
+            },
+            {
+                
             }
 
         ]
